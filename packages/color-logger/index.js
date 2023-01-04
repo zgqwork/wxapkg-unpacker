7756a1661e140b691a052204a7f52765047a22f6
zzdev <zzdev@developer.org>
2023-01-04 15:17:33 +0800

    v0.0.2-0

---
+++ packages/color-logger/index.js
@@ -0,0 +1,73 @@
+const Logger = require('js-logger')
+const colors = require('colors')
+const { basename } = require('path')
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red',
+})
+
+const DateTimeFormatOptions = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: false,
+}
+const locale = 'zh-CN'
+
+function getTime(locale) {
+  return new Date().toLocaleString(locale, DateTimeFormatOptions)
+}
+
+function getLogger(name) {
+  name = name || (require.main && basename(require.main.filename, '.js'))
+  return Logger.get(name)
+}
+
+Logger.useDefaults({
+  defaultLevel: Logger.TRACE,
+})
+Logger.setHandler((message, { level, name }) => {
+  message = Array.from(message)
+  name && message.unshift(`${name}`.cyan)
+  message.unshift(getTime(locale).grey)
+  const levelName = `[${level.name}]  `.slice(0, Logger.DEBUG.name.length + 2)
+  message.unshift(levelName)
+  // time
+  if (level === Logger.TIME) {
+    const [label, state] = message.slice(-2)
+    const prop = state === 'end' ? 'timeEnd' : 'time'
+    return console[prop](levelName + label)
+  }
+  // normal
+  const levelLowerCase = level.name.toLowerCase()
+  let msg = message.join(' ')
+  switch (level) {
+    case Logger.ERROR:
+      msg = msg[levelLowerCase].bold
+      break
+    case Logger.TRACE:
+      msg = [levelName, ...message.slice(3)].join(' ').cyan
+      break
+    default:
+      msg = msg[levelLowerCase]
+  }
+  console[levelLowerCase](msg)
+})
+
+if (!global.logger) {
+  global.logger = getLogger()
+}
+module.exports = {
+  getLogger,
+}
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 130,
+  arrowParens: 'avoid', //箭头函数括号
+  endOfLine: 'auto', //换行符 Linux or MacOs
+}
+// 相关链接 https://www.cnblogs.com/linjunfu/p/10880381.html
+// 相关链接 https://www.cnblogs.com/linjunfu/p/10755169.html
